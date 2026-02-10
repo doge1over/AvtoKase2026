04eac5b851fbabae90ba46c29dafddb8f38e6c7c
Даниил <daniilorunov@mail.ru>
2026-02-11 02:04:33 +0300

    5

--- app/components/YandexMetrika.tsx
+++ app/components/YandexMetrika.tsx
@@ -3,7 +3,6 @@
 import { useEffect } from 'react';
 import { usePathname, useSearchParams } from 'next/navigation';
 
-// Номер счётчика Яндекс Метрики
 const METRIKA_ID = 106768288;
 
 declare global {
@@ -12,7 +11,6 @@ declare global {
   }
 }
 
-// Функция для отправки целей
 export function trackGoal(goalName: string) {
   if (typeof window !== 'undefined' && window.ym) {
     window.ym(METRIKA_ID, 'reachGoal', goalName);
@@ -24,16 +22,14 @@ export default function YandexMetrika() {
   const searchParams = useSearchParams();
 
   useEffect(() => {
-    // Отслеживание переходов между страницами
     if (typeof window !== 'undefined' && window.ym) {
       window.ym(METRIKA_ID, 'hit', pathname);
     }
   }, [pathname, searchParams]);
 
   useEffect(() => {
-    // Отслеживание кликов по телефону
     const phoneLinks = document.querySelectorAll('a[href^="tel:"]');
-    
+
     const handlePhoneClick = () => {
       trackGoal('phone_click');
     };
@@ -49,40 +45,5 @@ export default function YandexMetrika() {
     };
   }, [pathname]);
 
-  return (
-    <>
-      <script
-        dangerouslySetInnerHTML={{
-          __html: `
-            (function(m,e,t,r,i,k,a){
-              m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-              m[i].l=1*new Date();
-              for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
-            })(window, document, 'script', 'https://mc.yandex.ru/metrika/tag.js?id=${METRIKA_ID}', 'ym');
-
-            ym(${METRIKA_ID}, 'init', {
-              ssr: true,
-              webvisor: true,
-              clickmap: true,
-              ecommerce: "dataLayer",
-              referrer: document.referrer,
-              url: location.href,
-              accurateTrackBounce: true,
-              trackLinks: true
-            });
-          `,
-        }}
-      />
-      <noscript>
-        <div>
-          <img 
-            src="https://mc.yandex.ru/watch/106768288" 
-            style={{ position: 'absolute', left: '-9999px' }} 
-            alt="" 
-          />
-        </div>
-      </noscript>
-    </>
-  );
+  return null;
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -330,6 +330,25 @@ export default function RootLayout({
             <link rel="icon" href="/favicon.ico" />
             <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
 
+            {/* Яндекс Метрика */}
+            <script
+                dangerouslySetInnerHTML={{
+                    __html: `
+                        (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                        m[i].l=1*new Date();
+                        for(var j=0;j<document.scripts.length;j++){if(document.scripts[j].src===r){return;}}
+                        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+                        (window,document,"script","https://mc.yandex.ru/metrika/tag.js","ym");
+
+                        ym(106768288,"init",{clickmap:true,trackLinks:true,accurateTrackBounce:true,webvisor:true});
+                    `,
+                }}
+            />
+            <noscript>
+                <div>
+                    <img src="https://mc.yandex.ru/watch/106768288" style={{ position: 'absolute', left: '-9999px' }} alt="" />
+                </div>
+            </noscript>
         </head>
         <body>
         {children}
